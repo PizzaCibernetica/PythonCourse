f91f42e77a3c8611974b2ef87973ca4f5343b83c
PizzaCibernetica <mspigolon.cyber@gmail.com>
2022-02-03 09:06:21 -0500

    still testing commit function in vscode

--- functions.py
+++ functions.py
@@ -12,3 +12,19 @@ print(bignum)
 
 #this is comment to be deleted
 
+# A parameter is a variable which we use in the function definition. It is a "handle" that 
+# allows the code in the function to access the arguments for a particular invocation.
+
+def greet(lang):
+    if lang == 'es':
+        print("Hola")
+    elif lang == 'fr':
+        print("Bonjour")
+    else:
+        print("Hello")
+
+greet("en")
+
+greet("es")
+
+greet("fr")
