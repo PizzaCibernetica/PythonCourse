09de441c9a301e0a3f456db5d162a80473559db6
PizzaCibernetica <mspigolon.cyber@gmail.com>
2022-02-03 08:56:27 -0500

    adig functions

--- files.py
+++ files.py
@@ -1,11 +1,30 @@
+score = input("Enter Score: ")
+fs = float(score)
+
+if fs > 1.0:
+    print("Out of range... above")
+    exit()
+elif fs < 0:
+    print("Oot of range... below")
+    exit()
+else:
+    if fs >= 0.9:
+        print("A") 
+    elif fs >= 0.8:
+        print("B")
+    elif fs >= 0.7:
+        print("C")
+    elif fs >= 0.6:
+        print("D")
+    else:
+        print("F")
+
+
 # file read
 
-fileHandle = open('filename.txt')
-for line in fileHandle:
-    line = line.rstrip()
-    if not '@icloud.com' in line:
-        continue
-    print(line)
-
-    
-
+#fileHandle = open('filename.txt')
+#for line in fileHandle:
+#    line = line.rstrip()
+#    if not '@icloud.com' in line:
+#        continue
+#    print(line)
---
+++ functions.py
@@ -0,0 +1,13 @@
+big = max("Hello world")
+
+small = min("Helloworld")
+
+print("Big" , big)
+print("Small" , small)
+
+
+bignum = max(1,2,3,4,5)
+
+print(bignum)
+
+
