6c7c08638043b23fb3885cf9239affd381838c39
PizzaCibernetica <mspigolon.cyber@gmail.com>
2022-02-04 15:03:51 -0500

    Definite loops

--- ch5LoopsAndIterations.py
+++ ch5LoopsAndIterations.py
@@ -70,5 +70,19 @@ print("Done!")
 # they will terminate or if they will be "infinite loops"
 
 # Sometimes it is a little harder to be sure if a loop will terminate 
-
+# -----------------------------------------------------------------
 # Definite Loops
+
+# Quite often we have a list of items of the lines in a file - 
+# effectively a finite set of things
+
+# We can write a loop to run once for each of the items in a set ising the python for construct
+
+# These loops are called "definite loops" because they execute an exact number of times
+
+# We say that "definite loops iterate through the members of a set"
+
+
+for i in [5,4,3,2,1] :
+    print(i)
+print("Blastoff!")
