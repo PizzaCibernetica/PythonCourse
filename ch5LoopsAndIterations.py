f67ed8de8f81b9f7bc96a71408452e40e44ea02b
PizzaCibernetica <mspigolon.cyber@gmail.com>
2022-02-04 14:21:56 -0500

    steps

--- ch5LoopsAndIterations.py
+++ ch5LoopsAndIterations.py
@@ -6,3 +6,28 @@ while n > 0 :
     n = n - 1
 print('Blastoff!')
 print(n)
+
+# Infinite loop is a loop that doesn't exit
+
+# example
+
+n = 5
+while n > 0 :
+    print("Lather")
+    print("Rinse")
+    exit() # needed to exit 
+print("Dry off!")
+
+# the following loop doesn't iterate at all
+
+# zero trip loop
+
+n = 0
+while n > 0 :
+    print("Lather")
+    print("Rinse")
+    exit() # needed to exit 
+print("Dry off!")
+
+# ways to exit loops
+
