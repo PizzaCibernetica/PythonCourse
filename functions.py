65f20a7ffa31eb71bbef18b3a1eebffc9dd0abc9
PizzaCibernetica <mspigolon.cyber@gmail.com>
2022-02-04 13:03:57 -0500

    added multiple parameters

--- functions.py
+++ functions.py
@@ -9,6 +9,7 @@ print("Small" , small)
 bignum = max(1,2,3,4,5)
 
 print(bignum)
+
 #this is comment to be deleted
 
 # A parameter is a variable which we use in the function definition. It is a "handle" that 
@@ -56,3 +57,18 @@ print(greet('en'), "Mike")
 print(greet("es"), "Mario")
 
 print(greet("fr"), "Sophie")
+
+# Mutiple parameters/Arguments
+
+#We can define more than one parameter in the function definition
+# We simply add more arguments when we call the function 
+#We match the nnumber and order of arguments and parameters
+
+def addtwo(a,b):
+    added = a + b
+    return added
+
+x = addtwo(3,5)
+print(x)
+
+print("The sum of 3 + 5 is ", x) 
