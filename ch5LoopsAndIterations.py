f253e96e2fa08a1d6ae5d2d1bdb2a45a008ce097
PizzaCibernetica <mspigolon.cyber@gmail.com>
2022-02-05 09:24:14 -0500

    Counting in a loop

--- ch5LoopsAndIterations.py
+++ ch5LoopsAndIterations.py
@@ -126,3 +126,18 @@ for the_num in [9,41,12,3,74,15]:
     print(largest_so_far, "-  -",the_num)
 
 print("After", largest_so_far)
+
+
+# Counting in a loop
+print("-- COUNTING IN A LOOP --")
+
+# To count how many times we execute a loop, we introduce a counter variable 
+# that starts at 0 and we add one to it each time through the loop.
+ 
+zork = 0
+print("Before", zork)
+for thing in [9,41,12,3,74,15]:
+    zork = zork + 1
+    print(zork, thing)
+print("After", zork)
+
