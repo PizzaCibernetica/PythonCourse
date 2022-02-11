5f8f27912476fb39e79d33e5ef9e1bcd92fe0732
PizzaCibernetica <mspigolon.cyber@gmail.com>
2022-02-11 17:19:59 -0500

    Dictionaries

--- ch9Dictionaries.py
+++ ch9Dictionaries.py
@@ -1,4 +1,7 @@
 # DICTIONARIES
+from pickle import LIST
+
+
 print('---  DICTIONARIES   ---')
 
 # What is a Collection?
@@ -11,5 +14,72 @@ print('---  DICTIONARIES   ---')
 
 # We have ways of finding the differnt places in the variable
 
+
+# What is not a collection?
+
+# most of our variables have one value in them - when we put a new value in the variable - the old value is overwritten
+
+#   $ pyhton
+#   >>> x = 2  
+#   >>> x = 4
+#   print(x)
+#   4               # the value is overwritten
+
+# comparing LISTS and DICTIONARIES
+
+ # List
+ # is organized. It is a linear collection of values that stay in order
+
+ # Dictionary
+ # are messier, there is no real sense of order in Dictionaries. It is like a "bag" of values, each with its own label
+
+
+
+ # Dictionaries 
+ # Dictionaries are Pyhton's most powerful collection
+ # Dictionaries allow us to do fast database-like operation in Python
+ # Dictionaries have different names in different languages
+ #      - Associative Arrays            : Perl/PHP
+ #      - Properties or Map or HashMap  : Java 
+ #      - Property Bag                  : C# / .NET
+ #
+
+ # Dictionaries
+
+ # lists index their entries based on the position in the list
+ # Dictionaries are like bags - no order
+ # # So we index the things we put in the dictionary with a "lookup tag"
+ # 
+
+# python
+# >>> purse = dict()
+# >>> purse['money'] = 12
+# >>> purse['candy'] = 3
+# >>> purse['tissue'] = 75
+# >>> print(purse)
+# {'money': 12, 'tissue': 75, 'candy': 3}
+# >>> print(purse['candy'])
+# 3
+# >>> purse['candy'] = purse['candy'] + 2
+# >>> print(purse)
+# {'money': 12, 'tissue': 75, 'candy': 5}
+# >>> exit()
+
+# Comparing Lists and Dictionaries
+
+# Dictionaries are like lists except that they use keys instead of numbers to look up values
+# both can have new items added to them
+# 
+
+# LIST                  VS      DICTIONARY
+
+# >>> lst = list()              >>> ddd = dict()
+# >>> lst.append(21)            >>> ddd['age'] = 21
+# >>> lst.append(183)           >>> ddd['course']=182
+# >>> print(lst)                >>> print(ddd)
+# [21, 183]                     {'course': 182, 'age': 21}
+# >>> lst[0] = 23               >>> ddd['age']=23
+# >>> print(lst)                >>> print(ddd)
+# [23, 183]                     {'course': 182, 'age': 23}
 
 
