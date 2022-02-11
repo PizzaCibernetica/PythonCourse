b047fc7e5bc171c897ea6365cb251dd7399133a5
PizzaCibernetica <mspigolon.cyber@gmail.com>
2022-02-11 16:28:09 -0500

    Dictionaries

--- ch9Dictionaries.py
+++ ch9Dictionaries.py
@@ -1,2 +1,15 @@
 # DICTIONARIES
 print('---  DICTIONARIES   ---')
+
+# What is a Collection?
+
+# A collection is nice because we can put more than one value in it and carry them all around in one convenient package
+
+# We have a bunch of values in a single "variable"
+
+# We do this by having more than one place "in" the variable
+
+# We have ways of finding the differnt places in the variable
+
+
+
