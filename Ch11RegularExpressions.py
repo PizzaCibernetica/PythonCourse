d30e077ddcd2fa936bbcbf1488a2e11ed2677d2c
PizzaCibernetica <mspigolon.cyber@gmail.com>
2022-02-15 20:33:23 -0500

    x

--- Ch11RegularExpressions.py
+++ Ch11RegularExpressions.py
@@ -1,3 +1,18 @@
 # --- REGULAR EXPRESSIONS ---
 
 print('---  Regular Expressions  ---')
+
+# In computing, a regular expression, also referred to as "regex" or 
+# "regexp", provides a concise and flexible means for matching strings
+#  of text, such as particular characters, words, or patterns of characters. 
+# A regular expression is written in a formal language that can be 
+# interpreted by a regular expression processor.
+
+# Really clever "wild card" expressions for matching and parsing strings.
+
+# UNDERSTANDING REGULAR EXPRESSIONS
+# very powerful and quite cryptic
+# fun once you understand them
+# Regular expressions are a language unto themselves
+# A lannguage of "marker characters" - programming with characters
+# It is kind of an "old school" language - compact
