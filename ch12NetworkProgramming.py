f5996e9b54be15934ddda878231c724e2a07839c
PizzaCibernetica <mspigolon.cyber@gmail.com>
2022-02-18 16:22:15 -0500

    beautiful soup

--- ch12NetworkProgramming.py
+++ ch12NetworkProgramming.py
@@ -293,6 +293,58 @@ print('\n--- Parsing HTML (a.k.a. Web Scraping) ---\n')
 # Search engines scrape web pages - we call this “spidering the web” or “web crawling”
 
 
+# Why Scrape?
+
+# Pull data - particularly social data - who links to who?
+# Get your own data back out of some system that has no “export capability”
+# Monitor a site for new information
+# Spider the web to make a database for a search engine
+
+# Scraping Web Pages
+
+# There is some controversy about web page scraping and some sites are a bit snippy about it.
+# Republishing copyrighted information is not allowed
+# Violating terms of service is not allowed
+
+
+# The Easy Way - Beautiful Soup
+
+# You could do string searches the hard way
+# Or use the free software library called BeautifulSoup from www.crummy.com
+
+
+# BeautifulSoup Installation
+
+# To run this, you can install BeautifulSoup
+# https://pypi.python.org/pypi/beautifulsoup4
+
+# Or download the file
+# http://www.py4e.com/code3/bs4.zip
+# and unzip it in the same directory as this file
+
+import urllib.request, urllib.parse, urllib.error
+from bs4 import BeautifulSoup
+
+...
+
+
+import urllib.request, urllib.parse, urllib.error
+from bs4 import BeautifulSoup                       # inport BeautifulSoup
+
+url = input('Enter - ')                             # Enter the URL
+html = urllib.request.urlopen(url).read()           # read the whole thing (we are not looping)
+soup = BeautifulSoup(html, 'html.parser')           # will parse and give back a soup object
+
+# Retrieve all of the anchor tags
+tags = soup('a')
+for tag in tags:
+    print(tag.get('href', None))
+
+
+
+
+
+
 
 
 
