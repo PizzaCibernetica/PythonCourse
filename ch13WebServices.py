0f9c1562d8ba127a5fdb9da60ed5cac2dfefa34a
PizzaCibernetica <mspigolon.cyber@gmail.com>
2022-02-22 08:27:32 -0500

    the wire

--- ch13WebServices.py
+++ ch13WebServices.py
@@ -1,3 +1,31 @@
 # Web Services
 print('---  Web Services  ---')
-# 
+# Data on the Web
+
+# With the HTTP Request/Response well understood and well supported,
+#  there was a natural move toward exchanging data between programs using these protocols
+# We needed to come up with an agreed way to represent data going between applications and across networks
+# There are two commonly used formats: XML and JSON
+
+
+# Sending Data Across the “Net”
+
+# a.k.a.  “Wire Protocol” - What we send on the “wire”
+# if we monitor the wire we would see this
+
+# Agreeing on a “Wire Format”
+
+# Python Dictionary    => serialize   ==> on the network ==> de-serialize ==> java HashMap
+# serialization (or serialisation) is the process of translating a data structure
+#  or object state into a format that can be stored or transmitted  in the "wire" and reconstructed later
+# wire protocol can be XML, JSON
+
+# JSON is simpler and easier 
+# XML is more precize 
+
+# XML - Extensible Markup Language
+
+
+
+
+
