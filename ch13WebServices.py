b0d209d153aa56f4400435e401abeef1c151cff0
PizzaCibernetica <mspigolon.cyber@gmail.com>
2022-02-22 08:38:11 -0500

    XML

--- ch13WebServices.py
+++ ch13WebServices.py
@@ -20,12 +20,33 @@ print('---  Web Services  ---')
 #  or object state into a format that can be stored or transmitted  in the "wire" and reconstructed later
 # wire protocol can be XML, JSON
 
-# JSON is simpler and easier 
+# JSON is simpler and easier
 # XML is more precize 
 
 # XML - Extensible Markup Language
+# eXtensible Markup Language
+
+# Primary purpose is to help information systems share structured data
+# It started as a simplified subset of the Standard Generalized Markup Language (SGML), and is designed to be relatively human-legible
+
+# in XML we get to name the tag what we choose
+
+# Start Tag             <person> <name>
+# End Tag               </person> </name>
+# Text Content          Chuck   +1 734 303 4456
+# Attribute             type="intl" 
+# Self Closing Tag      />
 
 
+# <person>
+#   <name>Chuck</name>
+#   <phone type="intl">
+#      +1 734 303 4456
+#    </phone>
+#    <email hide="yes" />
+# </person>
 
+# syntax errors in XML are more severe than in HTML , if you send bad XML the far end would not understand it
 
-
+# White Space
+# Line ends do not matter.  White space is generally discarded on text elements.  We indent only to be readable.
