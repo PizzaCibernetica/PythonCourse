41b7273d41242e2ca35529867df1668e2481306c
PizzaCibernetica <mspigolon.cyber@gmail.com>
2022-02-22 20:14:28 -0500

    Exercize 3 APIs

--- ch13WebServices.py
+++ ch13WebServices.py
@@ -429,17 +429,35 @@ print('--- Exercize 3 - Calling a JSON API ---')
 
 import urllib.request, urllib.parse, urllib.error
 import json
+import ssl
 
-serviceurl = 'http://py4e-data.dr-chuck.net/json?'
+api_key = False
+# If you have a Google Places API key, enter it here
+# api_key = 'AIzaSy___IDByT70'
+# https://developers.google.com/maps/documentation/geocoding/intro
+
+if api_key is False:
+    api_key = 42
+    serviceurl = 'http://py4e-data.dr-chuck.net/json?'
+else :
+    serviceurl = 'https://maps.googleapis.com/maps/api/geocode/json?'
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
 
 while True:
     address = input('Enter location: ')
     if len(address) < 1: break
 
-    url = serviceurl + urllib.parse.urlencode({'address': address})
+    parms = dict()
+    parms['address'] = address
+    if api_key is not False: parms['key'] = api_key
+    url = serviceurl + urllib.parse.urlencode(parms)
 
     print('Retrieving', url)
-    uh = urllib.request.urlopen(url)
+    uh = urllib.request.urlopen(url, context=ctx)
     data = uh.read().decode()
     print('Retrieved', len(data), 'characters')
 
@@ -453,8 +471,10 @@ while True:
         print(data)
         continue
 
-    lat = js["results"][0]["geometry"]["location"]["lat"]
-    lng = js["results"][0]["geometry"]["location"]["lng"]
+    print(json.dumps(js, indent=4))
+
+    lat = js['results'][0]['geometry']['location']['lat']
+    lng = js['results'][0]['geometry']['location']['lng']
     print('lat', lat, 'lng', lng)
     location = js['results'][0]['formatted_address']
     print(location)
