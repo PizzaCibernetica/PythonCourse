7e5112c70fc54878760c595f43c9c6e391f4c30d
PizzaCibernetica <mspigolon.cyber@gmail.com>
2022-02-24 11:07:13 -0500

    SQL

--- ch15databases.py
+++ ch15databases.py
@@ -59,4 +59,61 @@ print('- - -  Databases - - -')
 # -  MySql      : Simpler but very fast and scalable - commercial open source
 # -  SqlServer  : Very nice - from Microsoft (also Access)
 # Many other smaller projects, free and open source
-# -  HSQL, SQLite, Postgres, ... 
+# -  HSQL, SQLite, Postgres, MariaDB... 
+
+# --- SQLite Browser ---
+# SQLite is a very popular database - it is free and fast and small
+# SQLite Browser allows us to directly manipulate SQLite files
+# http://sqlitebrowser.org/
+# SQLite is embedded in Python and a number of other languages
+
+# --- SQL ---
+# Structured Query Language is the language we use to issue commands to the database
+# C R U D
+# -  Create data (a.k.a Insert)
+# -  Retrieve data
+# -  Update data
+# -  Delete data 
+
+
+# --- SQL: Insert ---
+# The insert statement inserts a row into a table
+
+# INSERT INTO Users (name, email) VALUES ('Kristin', 'kf@umich.edu')
+
+# --- SQL: Delete ---
+# Deletes a row in a table based on selection criteria
+
+# DELETE FROM Users WHERE email='ted@umich.edu'
+
+# --- SQL: Update ---
+# Allows the updating of a field with a where clause
+
+# UPDATE Users SET name='Charles' WHERE email='csev@umich.edu'
+
+# --- Retrieving Records: Select ---
+# The select statement retrieves a group of records - you can either
+#  retrieve all the records or a subset of the records with a WHERE clause
+
+# SELECT * FROM Users
+# SELECT * FROM Users WHERE email='csev@umich.edu'
+
+# --- Sorting with ORDER BY  ---
+# You can add an ORDER BY clause to SELECT statements to get the results sorted in ascending or descending order
+
+# SELECT * FROM Users ORDER BY email
+# SELECT * FROM Users ORDER BY name DESC
+
+
+
+
+# --- SQL Summary ---
+
+# INSERT INTO Users (name, email) VALUES ('Kristin', 'kf@umich.edu')
+# DELETE FROM Users WHERE email='ted@umich.edu'
+# UPDATE Users SET name="Charles" WHERE email='csev@umich.edu'
+# SELECT * FROM Users
+# SELECT * FROM Users WHERE email='csev@umich.edu'
+# SELECT * FROM Users ORDER BY email
+
+
